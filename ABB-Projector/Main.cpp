df4c1e8b606cdb5f69a3eb53c4d9ea733ccc942a
Asachun <chuchuchu.chunchun@gmail.com>
2017-06-04 21:12:42 +0900

    GUIを作成

--- ABB-Projector/Main.cpp
+++ ABB-Projector/Main.cpp
@@ -1,14 +1,56 @@
-﻿
-# include <Siv3D.hpp>
+﻿# include <Siv3D.hpp>
+
+GUI CreateGUI(const Vec2& scale, const Vec2& offset, int port)
+{
+	GUI gui(GUIStyle::Default);
+
+	gui.setTitle(L"Setting");
+	gui.addln(GUIText::Create(L"Scale"));
+	gui.add(GUIText::Create(L"("));
+	gui.add(L"scaleX", GUITextField::Create(5));
+	gui.add(GUIText::Create(L", "));
+	gui.add(L"scaleY", GUITextField::Create(5));
+	gui.addln(GUIText::Create(L")"));
+	gui.addln(GUIText::Create(L"Offset"));
+	gui.add(GUIText::Create(L"("));
+	gui.add(L"offsetX", GUITextField::Create(5));
+	gui.add(GUIText::Create(L", "));
+	gui.add(L"offsetY", GUITextField::Create(5));
+	gui.addln(GUIText::Create(L")"));
+	gui.addln(GUIText::Create(L"Network"));
+	gui.addln(L"state", GUIText::Create(L"State: Stop"));
+	gui.add(GUIText::Create(L"Port"));
+	gui.addln(L"port", GUITextField::Create(5));
+	gui.addln(L"connect", GUIButton::Create(L"Connect"));
+
+	gui.textField(L"scaleX").setText(ToString(scale.x));
+	gui.textField(L"scaleY").setText(ToString(scale.y));
+	gui.textField(L"offsetX").setText(ToString(offset.x));
+	gui.textField(L"offsetY").setText(ToString(offset.y));
+	gui.textField(L"port").setText(ToString(port));
+
+	return gui;
+}
+
+GUI CreateGUI()
+{
+	return CreateGUI(Vec2::One, Vec2::Zero, 50000);
+}
 
 void Main()
 {
-	const Font font(30);
+	Window::SetTitle(L"Augmented Building Blocks");
+	Window::SetStyle(WindowStyle::Sizeable);
+	Window::Maximize();
+
+	auto gui = CreateGUI();
 
 	while (System::Update())
 	{
-		font(L"ようこそ、Siv3D の世界へ！").draw();
-
-		Circle(Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
+		if (Input::KeySpace.clicked)
+		{
+			gui.show(!gui.style.visible);
+			Cursor::SetStyle(gui.style.visible ? CursorStyle::Default : CursorStyle::None);
+		}
 	}
 }
